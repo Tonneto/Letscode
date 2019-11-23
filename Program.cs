3eb50d83c2e8a9a0672293bdc6c718eb58ebbdc1
Ton Neto <tonsneto@gmail.com>
2019-11-22 21:07:44 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace brasfoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Time time =  new Time ("Insira o nome do clube: " + Console.ReadLine() ,   "Insira a cor do uniforme: " + Console.ReadLine());
+            
+        }
+    }
+}
