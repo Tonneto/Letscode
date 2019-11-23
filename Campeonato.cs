09d3b66efc0e39c775294662316291f12d9d55d6
Ton Neto <tonsneto@gmail.com>
2019-11-22 22:00:39 -0300

    Começo das Classes Time e Campeonato

---
+++ Campeonato.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+namespace brasfoot
+{
+
+    class NumTimes 
+    {
+        
+    }
+
+
+
+
+
+
+
+}
---
+++ Time.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+namespace brasfoot
+{
+    
+
+    class Time 
+    {
+        public string nome;
+        public string uniforme;
+
+        public void Grito() 
+        {
+            Console.WriteLine();
+        }
+
+        public Time(string nomeclube, string uniformeclube) 
+        {   
+            nomeclube = nome;
+            uniformeclube = uniforme;
+        }
+
+    }
+
+}
